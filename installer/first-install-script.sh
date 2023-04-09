4a28d94670deee3248440e346a0f30db79e3b239
anniekatz <91238138+anniekatz@users.noreply.github.com>
2023-04-08 21:32:39 -0400

    remove unnecessary packages

--- installer/first-install-script.sh
+++ installer/first-install-script.sh
@@ -14,7 +14,7 @@ echo "127.0.1.1 computer.localdomain computer" >> /etc/hosts
 # replace <PASSWORD> with chosen password
 echo root:<PASSWORD> | chpasswd
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils gvfs bash-completion rsync reflector acpi acpi_call acpid upower openbsd-netcat iptables-nft ipset firewalld flatpak os-prober ntfs-3g
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers xdg-user-dirs xdg-utils gvfs bash-completion rsync reflector acpi acpi_call acpid upower openbsd-netcat iptables-nft ipset firewalld os-prober ntfs-3g
 
 pacman -S nvidia xf86-video-intel nvidia-settings nvidia-utils
 
